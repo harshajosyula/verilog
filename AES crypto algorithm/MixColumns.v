5200b9c610621f1e5700504b9e1b2d7a8a6707fb
Harsha Josyula <josyulasriharsha@gmail.com>
2025-12-28 15:14:05 +0530

    Mix columns added

--- AES crypto algorithm/MixColumns.v
+++ AES crypto algorithm/MixColumns.v
@@ -3,7 +3,6 @@
     Mix columns
 
 */
-
 module MixCols(
     input wire[127:0] shiftedText,
     output wire[127:0] mixedCols
@@ -25,7 +24,7 @@ endgenerate
     b0_new = ( {02} * b0 + {03} * b1 + {01} * b2 + {01} * b3 )
     b1_new = ( {01} * b0 + {02} * b1+  {03} * b2 + {01} * b3 )
 
-    + in Galoie fields -> bitwise xor 
+    + in Galoei fields -> bitwise xor 
 
     * -> It follows that multiplication by x (i.e., {0000 0010} or {02}) 
     can be implemented at the byte level as a left shift and 
@@ -33,7 +32,6 @@ endgenerate
 
 */
 
-// functions  for multiplication
 
 function [7:0] xtime_by2;
     input [7:0] byte;
@@ -50,5 +48,40 @@ function [7:0] xtime_by1;
     end
 endfunction
 
+
+assign mixedArray[0] = xtime_by2(shiftedArray[0]) ^ xtime_by2(shiftedArray[1]) ^ shiftedArray[1] ^ shiftedArray[2] ^ shiftedArray[3] ;
+assign mixedArray[1] = shiftedArray[0] ^ xtime_by2(shiftedArray[1]) ^ xtime_by2(shiftedArray[2]) ^ shiftedArray[2] ^ shiftedArray[3] ;
+assign mixedArray[2] = shiftedArray [0] ^ shiftedArray[1] ^ xtime_by2(shiftedArray[2]) ^ xtime_by2(shiftedArray[3]) ^ shiftedArray[3] ;
+assign mixedArray[3] = xtime_by2(shiftedArray[0]) ^ shiftedArray[0] ^ shiftedArray[1] ^ shiftedArray[2] ^ xtime_by2(shiftedArray[3]);
+
+
+
+assign mixedArray[4] = xtime_by2(shiftedArray[4]) ^ xtime_by2(shiftedArray[5]) ^ shiftedArray[5] ^ shiftedArray[6] ^ shiftedArray[7] ;
+assign mixedArray[5] = shiftedArray[4] ^ xtime_by2(shiftedArray[5]) ^ xtime_by2(shiftedArray[6]) ^ shiftedArray[6] ^ shiftedArray[7] ;
+assign mixedArray[6] = shiftedArray [4] ^ shiftedArray[5] ^ xtime_by2(shiftedArray[6]) ^ xtime_by2(shiftedArray[7]) ^ shiftedArray[7] ;
+assign mixedArray[7] = xtime_by2(shiftedArray[4]) ^ shiftedArray[4] ^ shiftedArray[5] ^ shiftedArray[6] ^ xtime_by2(shiftedArray[7]);
+
+
+
+assign mixedArray[8] = xtime_by2(shiftedArray[8]) ^ xtime_by2(shiftedArray[9]) ^ shiftedArray[9] ^ shiftedArray[10] ^ shiftedArray[11] ;
+assign mixedArray[9] = shiftedArray[8] ^ xtime_by2(shiftedArray[9]) ^ xtime_by2(shiftedArray[10]) ^ shiftedArray[10] ^ shiftedArray[11] ;
+assign mixedArray[10] = shiftedArray [8] ^ shiftedArray[9] ^ xtime_by2(shiftedArray[10]) ^ xtime_by2(shiftedArray[11]) ^ shiftedArray[11] ;
+assign mixedArray[11] = xtime_by2(shiftedArray[8]) ^ shiftedArray[8] ^ shiftedArray[9] ^ shiftedArray[10] ^ xtime_by2(shiftedArray[11]);
+
+
+
+assign mixedArray[12] = xtime_by2(shiftedArray[12]) ^ xtime_by2(shiftedArray[13]) ^ shiftedArray[13] ^ shiftedArray[14] ^ shiftedArray[15] ;
+assign mixedArray[13] = shiftedArray[12] ^ xtime_by2(shiftedArray[13]) ^ xtime_by2(shiftedArray[14]) ^ shiftedArray[14] ^ shiftedArray[15] ;
+assign mixedArray[14] = shiftedArray [12] ^ shiftedArray[13] ^ xtime_by2(shiftedArray[14]) ^ xtime_by2(shiftedArray[15]) ^ shiftedArray[15] ;
+assign mixedArray[15] = xtime_by2(shiftedArray[12]) ^ shiftedArray[12] ^ shiftedArray[13] ^ shiftedArray[14] ^ xtime_by2(shiftedArray[15]);
+
+
+genvar k;
+
+generate for( k =0; k<16 ; k=k+1 )
+        begin
+        assign mixedCols[(127 - 8*k) -: 8] = mixedArray [k ]; 
+        end
+endgenerate
 
 endmodule
